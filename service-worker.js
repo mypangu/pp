1f806d693f1130c07ee3acdd72754ae8da3bcf7e
MrPangu <235312546+mypangu@users.noreply.github.com>
2026-06-18 12:00:07 +0530

    1806261200

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // panguplay PWA Service Worker
-const CACHE_NAME = "1806261147";
+const CACHE_NAME = "1806261200";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -58,7 +58,7 @@ self.addEventListener("fetch", event => {
   // Skip cross-origin requests and video files
   if (!event.request.url.startsWith(self.location.origin) ||
     event.request.url.match(/\.(mp4|webm|m3u8|mpd)/) ||
-    event.request.url.includes('linkplay.html')) ||
+    event.request.url.includes('linkplay.html') ||
     event.request.url.includes('player.html')) {
 	 return;
   }
